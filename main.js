4479f2a1224735b9c495544f246f9120897c173b
Mateusz Nurzyński <mateonurzynski@gmail.com>
2022-08-01 16:11:00 +0200

    Create a function that changes tile content

--- main.js
+++ main.js
@@ -8,16 +8,49 @@ const gameBoard = (function () {
 
 	function Tile(tileElement) {
 		return {
-			id: tileElement.getAttribute('id'),
+			id: tileElement.getAttribute('data-tile-id'),
 			content: tileElement.textContent,
 		};
 	}
 
 	function updateBoard() {
 		gameBoard.forEach((tile) => {
-			const currentTile = gameElement.querySelector(`#${tile.id}`);
+			const currentTile = gameElement.querySelector(
+				`[data-tile-id='${tile.id}']`
+			);
 			currentTile.textContent = tile.content;
 		});
 	}
 	updateBoard();
+
+	function checkAvailability(id) {
+		if (gameBoard[id].content === '') {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	function changeTile(id, newContent) {
+		const available = checkAvailability(id);
+		if (!available) {
+			console.log('Not Available');
+			return;
+		}
+		gameBoard[id].content = newContent;
+		updateBoard();
+	}
+
+	return { tileElements, changeTile };
+})();
+
+const inputControl = (function () {
+	gameBoard.tileElements.forEach((element) => {
+		element.addEventListener('click', handleClick);
+	});
+
+	function handleClick(e) {
+		const tileId = e.target.getAttribute('data-tile-id');
+		gameBoard.changeTile(tileId, 'X');
+	}
 })();
